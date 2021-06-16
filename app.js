ec26811ec776241533413f5c7202976509440fcc
eselse <ilikewebdevelopment@gmail.com>
2021-06-16 22:04:38 +0400

    add scripts & animation

---
+++ app.js
@@ -0,0 +1,26 @@
+// Define variables ================================================================
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+// Functions =======================================================================
+function breathAnimation() {
+  text.innerText = "Breath In!";
+  container.className = "container grow";
+
+  setTimeout(() => {
+    text.innerText = "Hold";
+
+    setTimeout(() => {
+      text.innerText = "Breathe Out!";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breathTime);
+}
+
+setInterval(breathAnimation, 7500);
